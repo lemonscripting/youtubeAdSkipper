b2b3f599ed4bf565044b2980631f179248f9ef1f
lemonscripting <147532946+lemonscripting@users.noreply.github.com>
2024-06-20 23:56:54 +0800

    add data to more.js

---
+++ more.js
@@ -0,0 +1,59 @@
+// ==UserScript==
+// @name         Youtube Auto Ad Skipper
+// @namespace    https://github.com/lemonscripting/
+// @version      1.6
+// @description  Automatically skips all types of ads present on youtube
+// @author       lemonscripting
+// @match        *://www.youtube.com/*
+// @license      BSD 3
+// @grant        none
+// ==/UserScript==
+
+const SPEED = 100;
+const mainVideo = document.querySelector('video.video-stream.html5-main-video');
+const frontPageBannerAd = document.getElementById('masthead-ad');
+
+setInterval(function () {
+    if (document.querySelector('.ytp-ad-player-overlay-layout__ad-info-container')) {
+        mainVideo.playbackRate = 16.0;
+        mainVideo.volume = 0.0;
+        var skipButton = document.querySelector('.ytp-skip-ad-button');
+        if (skipButton !== null && skipButton !== undefined) { // Check if skipButton exists
+            skipButton.click();
+        }
+    } else {
+        mainVideo.playbackRate = 1.0;
+        mainVideo.volume = 1.0;
+        mainVideo.style.display = 'block';
+    }
+
+    if (frontPageBannerAd) {
+        frontPageBannerAd.parentNode.removeChild(frontPageBannerAd);
+      console.log('Banner ad removed');
+    } else {
+      console.log('Banner ad does not exist');
+    }
+
+
+}, SPEED);
+
+
+
+//banner ad
+var frontPageBannerAd = document.getElementById('dismissible');
+if (frontPageBannerAd) {
+    frontPageBannerAd.parentNode.removeChild(frontPageBannerAd);
+  console.log('Banner ad removed');
+} else {
+  console.log('Banner ad does not exist');
+}
+
+//search sponsors
+var searchAds = document.querySelectorAll('div#rendering-content.style-scope.ytd-in-feed-ad-layout-renderer');
+searchAds.forEach(function(element) {
+    var parent = element.parentNode;
+    if (parent) {
+        parent.removeChild(element);
+    }
+});
+
